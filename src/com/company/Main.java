46e930e6a0dbdf71702e74b320c53ccf451c3a5b
clintonksang <sangclint0@gmail.com>
2021-09-29 00:27:16 +0300

    Bottles of Beer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,12 @@ public class Main {
             System.out.println("Take one down");
             System.out.println("Pass it around");
             beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println(beerNum +  word +  "of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            }
         }
     }
 }
